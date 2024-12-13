c9c638aee2e7a7085f59103ff50caf7c38d07851
slayer2324 <prasiddhabhattagcm@gmail.com>
2024-12-13 09:29:35 +0545

    first commit

---
+++ user.js
@@ -0,0 +1,14 @@
+//  usename="prasiddha bhatta"
+//  console.log(username)
+ var name='prasiddha bhatta'
+var email='prasiddhabhattagcm@gmail.com'
+ var roll_no= 34
+ 
+ var address='bansbari'
+ var phone= 9818953304
+
+ doucument.getElementById("name").innerHTML=name
+ doucument.getElementById("email").innerHTML=email
+ doucument.getElementById('rollno').innerHTML=roll_no
+ doucument.getElementById('address').innerHTML=address
+ doucument.getElementById('phone').innerHTML=phone
